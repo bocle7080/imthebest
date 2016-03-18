53b0b8e22c45337ee5d0bf4236791088593f018a
paco200926 <paco200926@gmail.com>
2016-03-18 08:43:30 -0700

    no0 change

--- js/game.js
+++ js/game.js
@@ -65,10 +65,10 @@ var update = function (modifier) {
 	}
 
 	if (
-		hero.x <= (monster.x + 32)
-		&& monster.x <= (hero.x + 32)
-		&& hero.y <= (monster.y + 32)
-		&& monster.y <= (hero.y + 32)
+		hero.x <= (monster.x + 14)
+		&& monster.x <= (hero.x + 40)
+		&& hero.y <= (monster.y + 16)
+		&& monster.y <= (hero.y + 40)
 	) {
 		++monstersCaught;
 		reset();
